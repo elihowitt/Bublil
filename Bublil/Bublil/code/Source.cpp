18b0a4b08e429c5b61b2508bcf4f66f469d2be9f
Eli <elihowitt1@gmail.com>
2018-12-03 13:11:41 +0200

    Added the frame but there are wierd err :(

--- Bublil/Bublil/code/Source.cpp
+++ Bublil/Bublil/code/Source.cpp
@@ -1,19 +1,40 @@
 #include"loggerMessages.h"
 #include"loggerUtil.h"
 
+#include"net\Client.h"
+#include"net\Server.h"
+
 int main()
 {
+	bool server;
 	lg::cwout::sayHello();
-	lg::dbout::basicLog(L"Hello output window");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	lg::dbout::basicLog(L"Bob`n Fop`n");
-	lg::cwout::warn("Bob`n!!!");
-	std::cin.get();
+	lg::cwout::generalLog("Question", "Open as server?");
+	std::cin >> server;
+	if (server)
+	{
+		Server MyServer(1111, false); //Create server on port 1111, false=do not loopback to localhost (others can connect)
+		while (true)
+		{
+			MyServer.ListenForNewConnection(); //Accept new connection (if someones trying to connect)
+		}
+	}
+	else//Client
+	{ 
+		Client myClient("127.0.0.1", 1111); //Create client to connect to server 127.0.0.1 [localhost] on port 1111
+		if (!myClient.Connect()) //If client fails to connect...
+		{
+			std::cout << "Failed to connect to server..." << std::endl;
+			system("pause");
+			return -1;
+		}
+		std::string userinput; //holds the user's chat message
+		while (true)
+		{
+			std::getline(std::cin, userinput); //Get line if user presses enter and fill the buffer
+			myClient.SendString(userinput); //Send string to server
+		}
+
+	}
 
 	return 0;
 }
